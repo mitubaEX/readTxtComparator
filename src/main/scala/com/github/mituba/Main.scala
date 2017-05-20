1fa77a75fabee2d76decf054ecf91cccc14d68a7
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2017-05-20 22:31:17 +0900

    書き方が悪かったので，for文で書いてみた

--- src/main/scala/com/github/mituba/Main.scala
+++ src/main/scala/com/github/mituba/Main.scala
@@ -8,15 +8,15 @@ import com.github.pochi.runner.scripts.ScriptRunnerBuilder;
 object Main{
   
 	def fileWrite(filename:String, birthmark:String) : String = {
-    val newFile = new PrintWriter("tmp/" + filename)
+    val newFile = new PrintWriter("tmp/" + filename + ".csv")
     newFile.write(filename + ",,," + birthmark)
     newFile.close()
-    "tmp/" + filename
+    "tmp/" + filename + ".csv"
 	}
 	
 	def compare(inputFilePath:String, filePath:String){
 	  val runner = new ScriptRunnerBuilder build
-	  val args = Array("compare_input_csv_test.js", "2-gram", inputFilePath, filePath)
+	  val args = Array("./compare_input_csv_test.js", "2-gram", "./"+ inputFilePath, "./" + filePath)
 	  runner.runsScript(args)
 	}
   
@@ -27,9 +27,10 @@ object Main{
 		for(s <- run(args(0))){
 		  val strSplit = s.split(",",3)
 		  if(strSplit(0) == "inputBirthmark"){
-        inputFilePath = fileWrite(strSplit(1), strSplit(2))
-      }else if(!strSplit(0).contains("Exception")){
-        compare(inputFilePath, fileWrite(strSplit(0), strSplit(2)))
+        inputFilePath = fileWrite("a", strSplit(2))
+      }else if(!strSplit(0).contains("Exception") && strSplit.length >= 3){
+        if(strSplit(1).toDouble >= 0.25)
+            compare(inputFilePath, fileWrite("b", strSplit(2)))
       }
 		}
   }

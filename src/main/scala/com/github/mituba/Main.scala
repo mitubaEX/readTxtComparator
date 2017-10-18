0df7bc7cf5894826f60b2c1e810a2b8f48087dc4
mituba <mituba@mituba-no-MacBook-Air.local>
2017-10-19 01:31:38 +0900

    ***でsplitして処理するように変更した

--- src/main/scala/com/github/mituba/Main.scala
+++ src/main/scala/com/github/mituba/Main.scala
@@ -6,32 +6,41 @@ import com.github.pochi.runner.scripts.ScriptRunner;
 import com.github.pochi.runner.scripts.ScriptRunnerBuilder;
 
 object Main{
-  
-	def fileWrite(filename:String, birthmark:String) : String = {
+
+  def fileWrite(filename:String, birthmark:String) : String = {
     val newFile = new PrintWriter("tmp/" + filename + ".csv")
-    newFile.write(filename + ",,," + birthmark)
+    newFile.write(birthmark)
     newFile.close()
     "tmp/" + filename + ".csv"
-	}
-	
-	def compare(inputFilePath:String, filePath:String){
-	  val runner = new ScriptRunnerBuilder build
-	  val args = Array("./compare_input_csv_test.js", "2-gram", "./"+ inputFilePath, "./" + filePath)
-	  runner.runsScript(args)
-	}
-  
+  }
+
+  def compare(inputFilePath:String, filePath:String){
+    val runner = new ScriptRunnerBuilder build
+    val args = Array("./compare_input_csv_test.js", "2-gram", "./"+ inputFilePath, "./" + filePath)
+    runner.runsScript(args)
+  }
+
   def run(arg : String) = scala.io.Source.fromFile(arg).getLines()
-  
+
   def main(args : Array[String]){
-    var inputFilePath = ""
-		for(s <- run(args(0))){
-		  val strSplit = s.split(",",3)
-		  if(strSplit(0) == "inputBirthmark"){
-        inputFilePath = fileWrite("a", strSplit(2))
-      }else if(!strSplit(0).contains("Exception") && strSplit.length >= 3){
-        if(strSplit(1).toDouble >= 0.25)
-            compare(inputFilePath, fileWrite("b", strSplit(2)))
+    for(s <- run(args(0))){
+      val splitString = s.split("\\*\\*\\*")
+      if(splitString.length >= 4){
+        val a = fileWrite("a", splitString(1))
+        val b = fileWrite("b", splitString(2))
+
+        compare(a, b)
+      } else {
+        println(s)
       }
-		}
+
+      // val strSplit = s.split(",",3)
+      // if(strSplit(0) == "inputBirthmark"){
+      //   inputFilePath = fileWrite("a", strSplit(2))
+      // }else if(!strSplit(0).contains("Exception") && strSplit.length >= 3){
+      //   if(strSplit(1).toDouble >= 0.25)
+      //     compare(inputFilePath, fileWrite("b", strSplit(2)))
+      // }
+    }
   }
 }
